721f2ea2e1285d19938b5299a64b9d81484c6f32
Nikolay Beketov <nbek@miko.ru>
2026-04-06 14:12:04 +0700

    Initial commit: ModuleGermanLanguagePack language pack for MikoPBX

---
+++ Messages/en/ModuleGermanLanguagePack.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'BreadcrumbModuleGermanLanguagePack' => 'German Language Pack',
+    'SubHeaderModuleGermanLanguagePack' => 'Complete German language support for MikoPBX',
+];
---
+++ Messages/ru/ModuleGermanLanguagePack.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'BreadcrumbModuleGermanLanguagePack' => 'Языковой пакет - Немецкий',
+    'SubHeaderModuleGermanLanguagePack' => 'Комплексная поддержка немецкого языка для системы MikoPBX',
+];
